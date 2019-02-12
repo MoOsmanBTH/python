46e82f03be53ae21c4892313ecfe35fa4bdf6ab4
MOHAMMED OSMAN <moos16@student.bth.se>
2019-02-12 14:49:44 +0100

    2.23

---
+++ DV1574/2.23.py
@@ -0,0 +1,22 @@
+def swap(element1, element2):
+    temp = element1
+    element1 = element2
+    element2 = temp
+
+alphabet = ["%s"%(chr(a))for a in range(ord("A"),ord("Z")+ 1, 1)]
+word = "PLAYFAIR"
+
+for a in range(0 len(word), 1):
+    swap(alphabet[a], aword[])
+
+
+alphabet.pop(9)    
+
+
+table = ""
+for a in range(0, 5, 1):
+    for b in range(0,5,1):
+        table += alphabet[a*5+b]
+    table += "\n"
+
+print(table)
