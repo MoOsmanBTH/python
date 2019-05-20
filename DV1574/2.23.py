1ab1c13cf1d9d232e5e29fdfde36d1217029fd2e
moOsmanBTH <moos16@student.bth.se>
2019-05-20 17:35:14 +0200

    .

--- DV1574/2.23.py
+++ DV1574/2.23.py
@@ -1,24 +1,23 @@
-#Inte klar
+# #Inte klar
 
-def swap(element1, element2):
-    temp = element1
-    element1 = element2
-    element2 = temp
+# def swap(element1, element2):
+#     temp = element1
+#     element1 = element2
+#     element2 = temp
 
-alphabet = ["%s"%(chr(a))for a in range(ord("A"),ord("Z")+ 1, 1)]
-word = "PLAYFAIR"
+# alphabet = ["%s"%(chr(a))for a in range(ord("A"),ord("Z")+ 1, 1)]
+# word = "PLAYFAIR"
 
-for a in range(0, len(word), 1):
-    swap(alphabet[a], aword[])
+# for a in range(0, len(word), 1):
+#     swap(alphabet[a], word[])
+
+# alphabet.pop(9)    
 
 
-alphabet.pop(9)    
+# table = ""
+# for a in range(0, 5, 1):
+#     for b in range(0,5,1):
+#         table += alphabet[a*5+b]
+#     table += "\n"
 
-
-table = ""
-for a in range(0, 5, 1):
-    for b in range(0,5,1):
-        table += alphabet[a*5+b]
-    table += "\n"
-
-print(table)
+# print(table)
